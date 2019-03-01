fb8635e63967a2ed5b73112137b01888c7f9f5b5
Arnob Saha <arnob.s@digitalroominc.com>
2019-02-28 23:19:26 -0600

    Completed the module

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
-
+app.use(express.urlencoded({ extended: true}));
 // get the account and user data
 const accountData = fs.readFileSync(
   path.join(__dirname,'json','accounts.json'),'UTF8');
@@ -25,6 +25,21 @@ app.get('/', (req,res) => res.render('index',{ title: 'Account Summary', account
 app.get('/savings', (req,res) => res.render('account', { account: accounts.saving }));
 app.get('/checking', (req,res) => res.render('account', { account: accounts.checking }));
 app.get('/credit', (req,res) => res.render('account', { account: accounts.credit }));
+app.get('/transfer', (req,res) => res.render('transfer'));
+app.post('/transfer', (req,res) => {
+  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.balance;
+  accounts[req.body.to].balance =  parseInt(accounts[req.body.to].balance) + parseInt(req.body.balance,10);
+  const accountsJSON = JSON.stringify(accounts,null,4);
+  fs.writeFileSync(path.join(__dirname,'json','accounts.json'),accountsJSON, 'UTF8');
+  res.render('transfer',{ message: 'Transfer Completed'});
+});
+app.get('/payment', (req,res) => res.render('payment', { account: accounts.credit }));
+app.post('/payment', (req,res) => {
+  accounts.credit.balance = req.body.balance;
+  const accountsJSON = JSON.stringify(accounts,null,4);
+  fs.writeFileSync(path.join(__dirname,'json','accounts.json'),accountsJSON, 'UTF8');
+  res.render('payment',{ message: 'Payment Completed', account: accounts.credit });
+});
 app.get('/profile', (req,res) => res.render('profile', { user: users[0] }));
 
 
