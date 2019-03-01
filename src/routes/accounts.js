73c316d5ad08fbc0c76b59a54d1b55b8d1c2d1a4
Arnob Saha <arnob.s@digitalroominc.com>
2019-03-01 01:34:40 -0600

    Completed the module

---
+++ src/routes/accounts.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+//declare the router module as variable by express
+const router = express.Router();
+
+const { accounts } = require('../data');
+
+router.get('/savings', (req,res) => res.render('account', { account: accounts.saving }));
+router.get('/checking', (req,res) => res.render('account', { account: accounts.checking }));
+router.get('/credit', (req,res) => res.render('account', { account: accounts.credit }));
